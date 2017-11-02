77c9f76e48c613d4300c4bcd00047520962fc645
alex4java <1920362327@qq.com>
2017-11-02 23:43:38 +0800

    构建服务操作信息

---
+++ hyts-application/src/main/java/com/hyts/app/data/model/DataModel.java
@@ -0,0 +1,31 @@
+/* 
+ * Copyright [2017] [Alex/LiBo(libo2dev.aliyun.com/alex.link@foxmail.com)]
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hyts.app.data.model;
+
+import com.hyts.jfmvc.scan.annotation.Entity;
+
+
+/**  
+ * @Title DataModel.java  
+ * @Package com.hyts.app.data.model  
+ * @Description TODO(用一句话描述该文件做什么)  
+ * @author LiBo/Alex  
+ * @date 2017年10月27日  
+ * @version V1.0  
+ */
+@Entity(tableName="TBL_DATA_MODEL")
+public class DataModel {
+
+}
